97b4962b7d6dadc063461d4ea077f6a2cd1a713b
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-13 14:16:51 +0100

    menu

---
+++ pages/include/footer.php
@@ -0,0 +1,24 @@
+
+
+<hr/>
+<footer>
+  <div id=" follow">
+    <div class="site">
+      <img class="icon" src="../images/fb.png" alt="icone facebook"/>
+      <p class="text"> Notre Facebook </p>
+    </div>
+    <div class="site">
+      <img class="icon" src="../images/twit.png" alt="icone twitter"/>
+      <p class="text"> Notre twitter </p>
+    </div>
+    <div class="site">
+      <img class="icon" src="../images/pint.png" alt="icone pinterest"/>
+      <p class="text"> Notre Pinterest </p>
+    </div>
+    <div class="site">
+      <img class="icon" src="../images/insta.png" alt="icone instagram"/>
+      <p class="text"> Notre Instagram </p>
+    </div>
+
+  </div>
+</footer>
---
+++ pages/include/menu.php
@@ -0,0 +1,69 @@
+
+
+          <div id="header">
+  			<div id="title">
+  				<h1> Pitted Skin </h1>
+  			</div>
+
+
+  			<div id="menu">
+
+  			<ul>
+
+  				<li>
+  					<a href="../Page/Accueil.html">Accueil</a>
+  				</li>
+
+  				<li>
+  					<a href="../Page/bijouxpiercings.html">Bijoux</a>
+  					<ul>
+  						<li><a href="bijouxpiercings.html">piercings</a></li>
+  						<li><a href="fauxpiercings.html">faux piercings</a></li>
+  						<li><a href="ecarteurs.html">ecarteurs</a></li>
+  						<li><a href="autresbijoux.html">autres</a></li>
+  					</ul>
+  				</li>
+
+  				<li>
+  					<a href="../Page/tarifs.html">Tarifs</a>
+  					<ul>
+  						<li><a href="tarifs.html">piercings</a></li>
+  						<li><a href="tarifstatoo.html">tatouages</a></li>
+  					</ul>
+  				</li>
+
+  				<li>
+  					<a href="../Page/tatoo.html">Tatouages</a>
+  					<ul>
+  						<li><a href="#">Noir et Blanc</a></li>
+  						<li><a href="#">Couleurs</a></li>
+  					</ul>
+  				</li>
+
+  				<li>
+  					<a href="../Page/photo.html">Photos</a>
+  					<ul>
+  						<li><a href="photopiercings.html">Piercings</a></li>
+  						<li><a href="phototatoo.html">Tatouages</a></li>
+  					</ul>
+  				</li>
+
+  				<li>
+  					<a href="#">Contacts</a>
+  					<ul>
+  						<li>
+  							<a href="shop.html">Horaires du shop</a>
+  						</li>
+  						<li>
+  							<a href="quisommesnous.html">Qui sommes nous ?</a>
+  						</li>
+  					</ul>
+  				</li>
+  				</li>
+
+
+  			</ul>
+
+  			</div>
+  		</div>
+  		<hr/>
