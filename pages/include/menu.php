036e1e93db6a4bef7886d9e293882de9233ff3a4
Cleliabrl <cleliaburle13@gmail.com>
2018-01-17 11:26:06 +0100

    menu

--- pages/include/menu.php
+++ pages/include/menu.php
@@ -5,7 +5,7 @@
               <h1>Pitted Skin</h1>
 
               <ul id="menu" style="list-style-type: none;">
-                 <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
+                 <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000 ;">
                   <a href="index.php" style=" text-decoration: none;">Accueil</a>
                  </li>
                   <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
