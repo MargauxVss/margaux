ebab8693d657191ad5281db78f1cc7ea6ca78b73
Cleliabrl <cleliaburle13@gmail.com>
2018-01-17 14:29:55 +0100

    hhbn

--- pages/include/menu.php
+++ pages/include/menu.php
@@ -5,21 +5,22 @@
               <img id="logo" src="../im/logopitedskin.jpg" alt="logo de pited skin"/>
 
               <ul id="menu" style="list-style-type: none;">
-                 <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000 ;">
+                 <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                   <a href="index.php" style=" text-decoration: none;">Accueil</a>
                  </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
+                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="bijoux.php" style=" text-decoration: none;">Bijoux</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
+                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="piercings.php" style=" text-decoration: none;">Piercings</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
+                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="tattoos.php" style=" text-decoration: none;">Tatouages</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ; color: #000000;">
+                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="contacts.php" style=" text-decoration: none;">Contacts</a>
                   </li>
               </ul>
+
           </header>
           <hr/>
