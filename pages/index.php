ad7373c746384d1f5e7adc5fca64f63e0b96bcbd
kevinmadonna <34097789+kevinmadonna@users.noreply.github.com>
2018-01-17 10:31:48 +0100

    f

--- pages/index.php
+++ pages/index.php
@@ -7,31 +7,19 @@
   <link rel="stylesheet" href="../style/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  <style>
-    #textindex
-    {
-      width: 800px; text-align: center; margin-top: 50px;
-    }
-    #textindex h2
-    {
-      text-decoration: underline overline #FF3028;
-    }
-    #textindex p
-    {
-      font-size: medium;
-    }
-  </style>
+  
 </head>
 <body>
 
-
+ 
   <?php include('./include/menu.php'); ?>
 
 
   <?php include('./include/formulaires.php'); ?>
+  
 
   <div id="page">
-  			<img id="imageprin" src="../im/peircing.png" alt=" image principale" />
+  			<img id="imageprin" src="../im/peircing.png" alt="image principale" />
   </div>
 
   <div id="textindex">
