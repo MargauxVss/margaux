f8b58f4737fa9be50415428d380093507d07dbcb
Cleliabrl <cleliaburle13@gmail.com>
2018-02-14 15:31:52 +0100

    efef"rg'r"gbgrtr"r

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//jQuery(document).ready(function($) {
+
+sfHover = function() {
+
+        var sfEls = document.getElementById("menu").getElementsByTagName("LI");
+
+        for (var i=0; i<sfEls.length; i++) {
+
+                sfEls[i].onmouseover=function() {
+
+                        this.className+=" sfhover";
+
+                }
+
+                sfEls[i].onmouseout=function() {
+
+                        this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+
+                }
+
+        }
+
+}
+
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+
+	
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); 
+} 
--- pages/include/menu.php
+++ pages/include/menu.php
@@ -1,26 +1,28 @@
 
 
-
+		<head>
+			 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+		</head>
           <header class="menu" style="text-align: center;">
-              <img id="logo" src="../im/logopitedskin.jpg" alt="logo de pited skin"/>
+              <img id="logo" src="../im/logo.png" alt="logo de pited skin"/>
 
               <ul id="menu" style="list-style-type: none;">
-                 <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                 <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                   <a href="index.php" style=" text-decoration: none;">Accueil</a>
                  </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="bijoux.php" style=" text-decoration: none;">Bijoux</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="piercings.php" style=" text-decoration: none;">Piercings</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="tattoos.php" style=" text-decoration: none;">Tatouages</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #F8F8FF ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="contacts.php" style=" text-decoration: none;">Contacts</a>
                   </li>
               </ul>
 
           </header>
-          <hr/>
+         
