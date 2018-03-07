48efe513569fa235ba2265b1bd23efc6bc04adc5
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2018-03-07 11:15:26 +0100

    Revert "efef"rg'r"gbgrtr"r"
    This reverts commit f8b58f4737fa9be50415428d380093507d07dbcb.

--- pages/index.php
+++ pages/index.php
@@ -2,25 +2,22 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8"/>
-<<<<<<< HEAD
-  <title>Accueil</title> 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-=======
   <title>Accueil</title>
->>>>>>> parent of f8b58f4... efef"rg'r"gbgrtr"r
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <title>Accueil</title>
   <link rel="stylesheet" href="../style/reset.css">
   <link rel="stylesheet" href="../style/style.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  
+
 </head>
 <body>
 
- 
+
   <?php include('./include/menu.php'); ?>
 
 
   <?php include('./include/formulaires.php'); ?>
-  
+
 
   <div id="page">
   			<img id="imageprin" src="../im/peircing.png" alt="image principale" />
@@ -36,14 +33,9 @@
         l'équipe a préférée revenir dans sa ville natale pour ouvrir à l' année leur studio de tatouage et de piercing.
       </p>
   </div>
-<<<<<<< HEAD
-</div>
- <?php include('./include/footer.php'); ?>
-=======
 
   <?php include('./include/footer.php'); ?>
 
->>>>>>> parent of f8b58f4... efef"rg'r"gbgrtr"r
   <script type="text/javascript" src="../js/script.js"></script>
 </body>
 </html>
