c7fea2edebab6f3c4ae07374b6904cda2d4fd9c5
MargauxVss <margauxvaissie@icloud.com>
2018-03-21 12:05:16 +0100

    page de connexion inscription a la place de tattoo[BYE BYE KEVIN]

--- pages/include/menu.php
+++ pages/include/menu.php
@@ -7,21 +7,27 @@
               <img id="logo" src="../im/logo.png" alt="logo de pited skin"/>
 
               <ul id="menu" style="list-style-type: none;">
+
                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                   <a href="index.php" style=" text-decoration: none;">Accueil</a>
                  </li>
+
                   <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="bijoux.php" style=" text-decoration: none;">Bijoux</a>
                   </li>
+
                   <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="piercings.php" style=" text-decoration: none;">Piercings</a>
                   </li>
-                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
-                    <a href="tattoos.php" style=" text-decoration: none;">Tatouages</a>
-                  </li>
+
                   <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
                     <a href="contacts.php" style=" text-decoration: none;">Contacts</a>
                   </li>
+
+                  <li style="display: inline; padding: 0 0.5em; background: #000 ; border: 1px solid #000000; font: 1em "Trebuchet MS",Arial,sans-serif ;">
+                  <a href="login.php" style=" text-decoration: none;"> Page de connexion / inscription </a>
+                  </li>
+                  
               </ul>
 
           </header>
---
+++ pages/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8"/>
+  <title>Tatouages</title>
+  <link rel="stylesheet" href="../style/reset.css">
+  <link rel="stylesheet" href="../style/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="../js/script.js"></script>
+</head>
+
+<body>
+
+<!-- formulaire d'inscription -->
+<form id="forminsc" action="inscription.php" method="post" style="border: 1px solid black; width: 120px; text-align: center;">
+    <h2> Inscription </h2>
+    <div>
+        <label style="font-size: small ; font-family: "verdana" ; , sans-serif;">Pseudo :</label>
+        <input type="text" name="pseudo" />
+    </div>
+    <div>
+        <label style="font-size: small ; font-family: "verdana", sans-serif;">Mail :</label>
+        <input type="email" name="mail" />
+    </div>
+    <div>
+        <label style="font-size: small ; font-family: "verdana", sans-serif;">Password :</label>
+        <input type="password" name="password"/>
+    </div>
+    <br/>
+    <div>
+        <input type="submit" name="envoyer" value="S'inscrire"/>
+    </div>
+</form>
+
+<br/>
+
+<!-- formulaire de connection -->
+<form id="formcon" action="connection.php" method="post" style="border: 1px solid black; width: 120px; text-align: center;">
+    <h2> Connexion </h2>
+    <div>
+        <label style="font-size: small ; font-family: "verdana", sans-serif;">Pseudo :</label>
+        <input type="text" name="pseudo" />
+    </div>
+    <div>
+        <label style="font-size: small ; font-family: "verdana", sans-serif;">Mail :</label>
+        <input type="email" name="mail" />
+    </div>
+    <div>
+        <label style="font-size: small ; font-family: "verdana", sans-serif;">Password :</label>
+        <input type="password" name="password"/>
+    </div>
+    <br/>
+    <div>
+        <input type="submit" name="envoyer" value="Se connecter"/>
+    </div>
+</form>
+
+
+  <?php include('./include/footer.php'); ?>
+
+</body>
+
+</html>
