e58ee11f6fdc067bb5022f07922bc191e9f4a411
MargauxVss <margauxvaissie@icloud.com>
2018-04-04 16:37:36 +0200

    mise en forme page mentions

--- pages/mentions.php
+++ pages/mentions.php
@@ -5,15 +5,14 @@
 
     <meta charset="UTF-8"/>
     <title>Mentions légales</title>
-    <link rel="stylesheet" href="../../style/reset.css">
-    <link rel="stylesheet" href="../../style/style.css">
-    <link rel="stylesheet" href="../../bijouxtemplate/css/heroix-features.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/reset.css">
+    <link rel="stylesheet" href="../style/style.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php include('./menu.php'); ?>
+<?php include('./include/menu.php'); ?>
 
 
 <!-- Page Content -->
@@ -48,8 +47,8 @@
 
 
 
-<?php include('./footer.php'); ?>
-<script type="text/javascript" src="../../script/gulpfile.js"></script>
+<?php include('./include/footer.php'); ?>
+<script type="text/javascript" src="../script/gulpfile.js"></script>
 
 
 
